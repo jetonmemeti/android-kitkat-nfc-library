6e7efb18a179ce273df80402cd6387dc5804abe5
Jeton Memeti <jeton.memeti@gmail.com>
2014-04-08 10:47:17 +0200

    Implemented ProtocolMessage and NfcMessage.

---
+++ AndroidKitKatNFCLibrary/src/ch/uzh/ch/nfclib/messages/NfcMessage.java
@@ -0,0 +1,77 @@
+package ch.uzh.ch.nfclib.messages;
+
+
+/**
+ * This is the lower layer protocol message. It is responsible for sending
+ * Messages between two devices via NFC. It is build to allow message
+ * fragmentation and reassembly, based on the communication device's NFC message
+ * size capabilities. The status values in the header can be combined (OR, AND,
+ * etc.) to transmit more than one status to the counterpart. The header
+ * contains also a sequence number in order to detect multiple transmission of
+ * the same messages or the lost of a message.
+ */
+public class NfcMessage extends ProtocolMessage {
+	
+	public static final byte AID_SELECTED =	0x01;
+	public static final byte START_PROTOCOL = 0x02;
+	public static final byte RETRANSMIT = 0x03;
+	public static final byte HAS_MORE_FRAGMENTS = 0x08;
+	public static final byte GET_NEXT_FRAGMENT = 0x10;
+	public static final byte WAIT_FOR_ANSWER = 0x20;
+	
+	public static final int HEADER_LENGTH = 2;
+	
+	public NfcMessage(byte[] data) {
+		setHeaderLength(HEADER_LENGTH);
+		setData(data);
+	}
+	
+	/**
+	 * Creates a new NfcMessage.
+	 * 
+	 * @param status
+	 *            the status to be contained in the header
+	 * @param sequenceNumber
+	 *            the sequence number of this message to be contained in the
+	 *            header
+	 * @param payload
+	 *            the payload of this message
+	 */
+	public NfcMessage(byte status, byte sequenceNumber, byte[] payload) {
+		byte[] data;
+		if (payload != null && payload.length > 0) {
+			data = new byte[payload.length+HEADER_LENGTH];
+			System.arraycopy(payload, 0, data, HEADER_LENGTH, payload.length);
+		} else {
+			data = new byte[HEADER_LENGTH];
+		}
+		
+		data[0] = status;
+		data[1] = sequenceNumber;
+		
+		setHeaderLength(HEADER_LENGTH);
+		setData(data);
+	}
+	
+	/**
+	 * Returns the sequence number of this NfcMessage. If the data is null, then
+	 * 0xFF is returned.
+	 */
+	public byte getSequenceNumber() {
+		if (getData() == null || getData().length == 0)
+			return (byte) 0xFF;
+		else
+			return getData()[1];
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("NfcMessage: ");
+		sb.append("head: ");
+		sb.append(", status: ").append(Integer.toHexString(getData()[0] & 0xFF));
+		sb.append(", sequence: ").append(Integer.valueOf(getData()[1] & 0xFF));
+		sb.append("/ payload length:").append(getData().length-HEADER_LENGTH);
+		return sb.toString();
+	}
+	
+}
---
+++ AndroidKitKatNFCLibrary/src/ch/uzh/ch/nfclib/messages/ProtocolMessage.java
@@ -0,0 +1,84 @@
+package ch.uzh.ch.nfclib.messages;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * This is the abstract base type of the protocol messages send via NFC to
+ * accomplish a transaction. The ProtocolMessage consists of a header and an
+ * arbitrary long payload.
+ */
+public abstract class ProtocolMessage {
+	
+	/*
+	 * The data consists of the header plus payload
+	 */
+	private byte[] data;
+	private int headerLength;
+	
+	/**
+	 * Returns the status code contained in the first byte of the header. Each
+	 * {@link ProtocolMessage} has in its first byte of the header some kind of
+	 * status information of that message. If the data is null, then 0xFF is
+	 * returned.
+	 */
+	public byte getStatus() {
+		if (data == null)
+			return (byte) 0xFF;
+		else
+			return data[0];
+	}
+	
+	/**
+	 * Sets the status of this ProtocolMessage.
+	 */
+	public void setStatus(byte status) {
+		if (data != null && data.length > 0)
+			data[0] = status;
+	}
+	
+	/**
+	 * Sets the header length, i.e., how many bytes in the data belong to the
+	 * header.
+	 * 
+	 * @param length
+	 *            the length of the header
+	 */
+	public void setHeaderLength(int length) {
+		headerLength = length;
+	}
+	
+	/**
+	 * Returns the complete data (header plus payload).
+	 */
+	public byte[] getData() {
+		return data;
+	}
+	
+	/**
+	 * Sets the data, consisting of the header plus payload.
+	 */
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	
+	/**
+	 * Returns the payload only (excluding the header) or null.
+	 */
+	public byte[] getPayload() {
+		if (data == null)
+			return null;
+		else
+			return ArrayUtils.subarray(data, headerLength, data.length);
+	}
+	
+	/**
+	 * Returns the length of the payload.
+	 */
+	public int getPayloadLength() {
+		if (data == null || data.length == 0)
+			return 0;
+		else
+			return data.length-headerLength; 
+	}
+	
+}
