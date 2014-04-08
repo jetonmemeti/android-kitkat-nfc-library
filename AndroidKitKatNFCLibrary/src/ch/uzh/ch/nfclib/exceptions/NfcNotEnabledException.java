36ece1348ad041f8e23ae70e0ab73991a553e950
Jeton Memeti <jeton.memeti@gmail.com>
2014-04-08 14:01:23 +0200

    Added two Exception classes.

---
+++ AndroidKitKatNFCLibrary/src/ch/uzh/ch/nfclib/exceptions/NfcNotEnabledException.java
@@ -0,0 +1,6 @@
+package ch.uzh.ch.nfclib.exceptions;
+
+public class NfcNotEnabledException extends Exception {
+	private static final long serialVersionUID = -8780373763513187959L;
+	
+}
---
+++ AndroidKitKatNFCLibrary/src/ch/uzh/ch/nfclib/exceptions/NoNfcException.java
@@ -0,0 +1,6 @@
+package ch.uzh.ch.nfclib.exceptions;
+
+public class NoNfcException extends Exception {
+	private static final long serialVersionUID = -3870421490219231959L;
+	
+}
