d24de75e838822f8ca75e2415c7b2c2c21a4fbbc
Jeton Memeti <jeton.memeti@gmail.com>
2014-04-20 21:56:20 +0200

    Fix bug in NfcMessage.

--- AndroidKitKatNFCLibrary/src/ch/uzh/csg/nfclib/messages/NfcMessage.java
+++ AndroidKitKatNFCLibrary/src/ch/uzh/csg/nfclib/messages/NfcMessage.java
@@ -60,7 +60,7 @@ public class NfcMessage extends ProtocolMessage {
 	 * 0xFF is returned.
 	 */
 	public byte getSequenceNumber() {
-		if (getData() == null || getData().length == 0)
+		if (getData() == null || getData().length < 1)
 			return (byte) 0xFF;
 		else
 			return getData()[1];
