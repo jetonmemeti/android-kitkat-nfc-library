34bdb4d030a790f10e3396183aee19e0417b5c94
Jeton Memeti <jeton.memeti@gmail.com>
2014-04-29 14:29:31 +0200

    Added TODO in InternalNfcTransceiver.

--- AndroidKitKatNFCLibrary/src/ch/uzh/csg/nfclib/transceiver/InternalNfcTransceiver.java
+++ AndroidKitKatNFCLibrary/src/ch/uzh/csg/nfclib/transceiver/InternalNfcTransceiver.java
@@ -122,6 +122,7 @@ public class InternalNfcTransceiver extends NfcTransceiver implements ReaderCall
 //		
 //	}
 
+	//TODO: used?
 	private void cancel() {
 		if (isoDep != null && isoDep.isConnected()) {
 			try {
